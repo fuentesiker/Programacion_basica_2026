f19fb22101fbb50933515177a251edf9c920aacf
fuentesiker <fuentesiker50@gmail.com>
2026-03-06 20:56:33 -0500

    Create ejercicio1.py

---
+++ semana3/ejercicio1.py
@@ -0,0 +1,17 @@
+#esto es un tipo de dato float
+a=2
+#esto es un tipo de dato entero
+b=200
+print( "el resultado de esta suma es:", a+b)
+print("el resultado de esta resta es:",a-b)
+print("el resultado de esta multiplicacion es:",a*b)
+#esta division da en decimales
+print("el resultado de esta division es:",a/b)
+#esta divison da en enteros
+print("el resultado de esta division de resultado entero es:",a//b)
+#es el reciduo de la division
+print("el resultado de este residuo es:",a%b)
+#comparaciones
+print(a<b)
+print(a>b)
+print(a==b)
